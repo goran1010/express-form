4a5378108beb4f80848e42098029324b3e3d62e2
Goran Jovic <goran1010jovic@gmail.com>
2025-05-25 13:04:16 +0200

    backup commit

---
+++ controllers/usersController.js
@@ -0,0 +1,106 @@
+const usersStorage = require("../storages/usersStorage");
+
+exports.usersListGet = (req, res) => {
+  res.render("index", {
+    title: "User list",
+    users: usersStorage.getUsers(),
+  });
+};
+
+exports.usersCreateGet = (req, res) => {
+  res.render("createUser", {
+    title: "Create user",
+  });
+};
+
+const { body, validationResult } = require("express-validator");
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+
+const validateUser = [
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`First name ${lengthErr}`),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Last name ${lengthErr}`),
+  body("email").trim().notEmpty().isEmail().withMessage(`Must be valid email`),
+  body("age")
+    .trim()
+    .optional({ values: "falsy" })
+    .isInt({ min: 18, max: 120 })
+    .withMessage(`Must be a number between 18 and 120`),
+  body("bio")
+    .trim()
+    .optional({ values: "falsy" })
+    .isLength({ max: 200 })
+    .withMessage(`Max length 200 characters`),
+];
+// We can pass an entire array of middleware validations to our controller.
+exports.usersCreatePost = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("createUser", {
+        title: "Create user",
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName, email, age, bio } = req.body;
+    usersStorage.addUser({ firstName, lastName, email, age, bio });
+    res.redirect("/");
+  },
+];
+exports.usersUpdateGet = (req, res) => {
+  const user = usersStorage.getUser(req.params.id);
+  res.render("updateUser", {
+    title: "Update user",
+    user: user,
+  });
+};
+
+exports.usersUpdatePost = [
+  validateUser,
+  (req, res) => {
+    const user = usersStorage.getUser(req.params.id);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("updateUser", {
+        title: "Update user",
+        user: user,
+        errors: errors.array(),
+      });
+    }
+    const { firstName, lastName, email, age, bio } = req.body;
+    usersStorage.updateUser(req.params.id, {
+      firstName,
+      lastName,
+      email,
+      age,
+      bio,
+    });
+    res.redirect("/");
+  },
+];
+// Tell the server to delete a matching user, if any. Otherwise, respond with an error.
+exports.usersDeletePost = (req, res) => {
+  usersStorage.deleteUser(req.params.id);
+  res.redirect("/");
+};
+
+exports.usersSearch = (req, res) => {
+  const user = usersStorage.getUserByName(req.query.search);
+  console.log(user);
+  res.render("search", {
+    title: "Search user",
+    user: user,
+  });
+};
